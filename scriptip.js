be0a3d5e49d71f27faa98af5f595a878d798fc5a
drbynz0 <infosastuce78@gmail.com>
2024-11-17 00:56:40 +0100

    yidgg

--- scriptip.js
+++ scriptip.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll('.diplome, .experience, .competence');
+    const sections = document.querySelectorAll('.section-bloc');
 
-    sections.forEach(function(section) {
-        const toggleIcon = section.querySelector('span i');
-        const sousSection = section.querySelector('.sous-section');
+    sections.forEach(function(sectionBloc) {
+        const toggleIcon = sectionBloc.querySelector('span i');
+        const sousSection = sectionBloc.querySelector('.sous-section');
 
+        const sousSectionHeight = sousSection.scrollHeight;
+
+        toggleIcon.style.transform = 'rotate(180deg)';
+        sousSection.classList.remove('ssactive');
+        sectionBloc.style.height = '100px';
         sousSection.style.display = 'flex';
 
+
         toggleIcon.addEventListener('click', function() {
-            if (sousSection.style.display === 'flex') {
-                sousSection.style.display = 'none';
+            if (sousSection.classList.contains('ssactive')) {
+                sectionBloc.style.height = '100px';
+                sousSection.classList.remove('ssactive');
                 toggleIcon.style.transform = 'rotate(180deg)';
             } else {
-                sousSection.style.display = 'flex';
+                sectionBloc.style.height = `${sousSectionHeight + 100}px`;
+                sousSection.classList.add('ssactive');
                 toggleIcon.style.transform = 'rotate(0deg)';
             }
         });
